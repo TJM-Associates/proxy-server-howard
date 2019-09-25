f46313e383bdea8ccfdf9c51842d215a3efbd91f
Jason Chen <jason.sl.chen@gmail.com>
2019-09-25 10:58:12 -0700

    made proxy on my service smart proxy, added in group members deploysed services

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
@@ -5,6 +6,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(compression());
 app.use('/restaurants/:rid', express.static(path.resolve(__dirname, '../public')));
+
+app.get('/api/restaurants/:rid/images', (req, res) => {
+  res.redirect(`http://54.193.42.82:3002/api/restaurants/${req.params.rid}/images`)
+})
+
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
