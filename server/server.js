874b3e512acbaeceff2289ae0fbc4425b0170d71
Jason Chen <jason.sl.chen@gmail.com>
2019-09-25 11:01:12 -0700

    added loader folder and loaderio server up file

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(compression());
 app.use('/restaurants/:rid', express.static(path.resolve(__dirname, '../public')));
 
+app.use('/', express.static(path.resolve(__dirname, '../loader')));
+
+
 app.get('/api/restaurants/:rid/images', (req, res) => {
   res.redirect(`http://54.193.42.82:3002/api/restaurants/${req.params.rid}/images`)
 })
